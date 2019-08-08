2ee94d3fed66e7dc77a6a6ce9ec5d729154514d6
Alexander Strakhovenko <sasstr@gmail.com>
2019-08-08 18:41:13 +0300

    Баг фиксинг тревис

--- src/main.js
+++ src/main.js
@@ -1,36 +1,40 @@
+'use strict';
+
+const CARD_COUNT = 3;
+
 /**
  * Функция возращает html разметку пунктов меню.
  * @return string
  */
 const getMenu = () => {
-return `
-<section class="control__btn-wrap">
-  <input
-    type="radio"
-    name="control"
-    id="control__new-task"
-    class="control__input visually-hidden"
-  />
-  <label for="control__new-task" class="control__label control__label--new-task"
-    >+ ADD NEW TASK</label
-  >
-  <input
-    type="radio"
-    name="control"
-    id="control__task"
-    class="control__input visually-hidden"
-    checked
-  />
-  <label for="control__task" class="control__label">TASKS</label>
-  <input
-    type="radio"
-    name="control"
-    id="control__statistic"
-    class="control__input visually-hidden"
-  />
-  <label for="control__statistic" class="control__label"
-    >STATISTICS</label>
-</section>`;
+  return `
+  <section class="control__btn-wrap">
+    <input
+      type="radio"
+      name="control"
+      id="control__new-task"
+      class="control__input visually-hidden"
+    />
+    <label for="control__new-task" class="control__label control__label--new-task"
+      >+ ADD NEW TASK</label
+    >
+    <input
+      type="radio"
+      name="control"
+      id="control__task"
+      class="control__input visually-hidden"
+      checked
+    />
+    <label for="control__task" class="control__label">TASKS</label>
+    <input
+      type="radio"
+      name="control"
+      id="control__statistic"
+      class="control__input visually-hidden"
+    />
+    <label for="control__statistic" class="control__label"
+      >STATISTICS</label>
+  </section>`;
 };
 
 /**
@@ -38,16 +42,16 @@ return `
  * @return string
  */
 const getSearch = () => {
-return `<section class="main__search search container">
-<input
-  type="text"
-  id="search__input"
-  class="search__input"
-  placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
-/>
-<label class="visually-hidden" for="search__input">Search</label>
-</section>`;
-};
+  return `<section class="main__search search container">
+  <input
+    type="text"
+    id="search__input"
+    class="search__input"
+    placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
+  />
+  <label class="visually-hidden" for="search__input">Search</label>
+  </section>`;
+  };
 
 /**
  * Функция возращает html разметку фильтра.
@@ -534,7 +538,6 @@ renderComponent(board, getBoardTasks());
 const boardTasks = document.querySelector('.board__tasks');
 renderComponent(boardTasks, getCardEditForm());
 
-const CARD_COUNT = 3;
 for (let i = 0; i < CARD_COUNT; i++) {
   renderComponent(boardTasks, getCard());
 }
