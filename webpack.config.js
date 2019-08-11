0dff0b5850e4945ed6550e4eae40898a7662ab91
Alexander Strakhovenko <sasstr@gmail.com>
2019-08-11 13:16:43 +0300

    Выбросил мусор из конфига вебпака

--- webpack.config.js
+++ webpack.config.js
@@ -6,23 +6,12 @@ module.exports = {
     filename: `bundle.js`,
     path: path.join(__dirname, `public`)
   },
-  /* module: {
-    rules: [
-      {
-          test: /\.js$/,
-          exclude: '/node_modules/'
-      }
-    ]
-  } ,*/
   devtool: `source-map`,
   devServer: {
     contentBase: path.join(__dirname, `public`), // Где искать сборку
     publicPath: `http://localhost:8080/`, // Веб адрес сборки
     compress: true, // Сжатие
     overlay: true, // Выводит ошибки на экране.
-    // Автоматическая перезагрузка страницы
-    // Если не работает по стандартному URLу в браузере ‘http://localhost:8080’,
-    // то добавьте к нему ‘/webpack-dev-server/‘: ‘http://localhost:8080/webpack-dev-server/'
     watchContentBase: true
   }
 };
